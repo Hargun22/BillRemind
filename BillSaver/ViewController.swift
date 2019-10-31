a471de69762849eb0a2026c7bb35ffae44b90729
Hargun <hargunbedi463@gmail.com>
2019-10-31 11:01:35 -0400

    Initial Commit

---
+++ BillSaver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BillSaver
+//
+//  Created by Hargun Bedi on 2019-10-31.
+//  Copyright © 2019 Hargun Bedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BillSaverTests/BillSaverTests.swift
@@ -0,0 +1,34 @@
+//
+//  BillSaverTests.swift
+//  BillSaverTests
+//
+//  Created by Hargun Bedi on 2019-10-31.
+//  Copyright © 2019 Hargun Bedi. All rights reserved.
+//
+
+import XCTest
+@testable import BillSaver
+
+class BillSaverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
